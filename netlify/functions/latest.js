ad3b4171ffa49c4ecbdb0a21effd823d619fa80b
er3n-g <supercatsgaming0@gmail.com>
2025-06-07 14:14:15 +0300

    Update latest.js

--- netlify/functions/latest.js
+++ netlify/functions/latest.js
@@ -1,1 +1,19 @@
+let lastSensorValue = "No data yet";
+
+exports.handler = async (event, context) => {
+  if (event.httpMethod === "POST") {
+    const { sensor } = JSON.parse(event.body || "{}");
+    lastSensorValue = sensor || lastSensorValue;
+    return {
+      statusCode: 200,
+      body: "Sensor data saved.",
+    };
+  } else if (event.httpMethod === "GET") {
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ sensor: lastSensorValue }),
+    };
+  }
+};
 
