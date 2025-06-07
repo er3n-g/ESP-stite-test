b6be5c9c9e80b8197d182a5125ae66d68496a7ec
er3n-g <supercatsgaming0@gmail.com>
2025-06-07 13:56:51 +0300

    Update submit.js

--- netlify/functions/submit.js
+++ netlify/functions/submit.js
@@ -1,3 +1,11 @@
-{
-  "command": "0"
-}
+exports.handler = async (event, context) => {
+  const sensorValue = event.queryStringParameters.sensor;
+
+  // For now, just log the value. You can save it to a file or database later.
+  console.log("Received sensor value:", sensorValue);
+
+  return {
+    statusCode: 200,
+    body: `Sensor value received: ${sensorValue}`,
+  };
+};
