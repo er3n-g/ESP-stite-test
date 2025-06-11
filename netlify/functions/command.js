365f5d1db3c710a13ad038cb762894159baabbc7
er3n-g <supercatsgaming0@gmail.com>
2025-06-11 20:07:22 +0300

    Update command.js

--- netlify/functions/command.js
+++ netlify/functions/command.js
@@ -1,1 +1,23 @@
+// set-command.js
+let currentCommand = "";
+
+exports.handler = async (event) => {
+  if (event.httpMethod === "POST") {
+    const { command } = JSON.parse(event.body || "{}");
+    currentCommand = command;
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Command updated", command }),
+    };
+  }
+
+  if (event.httpMethod === "GET") {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ command: currentCommand }),
+    };
+  }
+
+  return { statusCode: 405, body: "Method Not Allowed" };
+};
 
