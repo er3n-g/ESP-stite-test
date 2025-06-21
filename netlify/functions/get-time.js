b512a3976ac10b6f22917df615f33f2cbf2e6593
er3n-g <supercatsgaming0@gmail.com>
2025-06-21 16:02:30 +0300

    Update get-time.js

--- netlify/functions/get-time.js
+++ netlify/functions/get-time.js
@@ -1,9 +1,14 @@
 exports.handler = async () => {
   const now = new Date();
-  const hours = now.getHours().toString().padStart(2, '0');
-  const minutes = now.getMinutes().toString().padStart(2, '0');
+
+  // Israel time = UTC + 3 hours
+  const offsetMinutes = 3 * 60; // 180 minutes
+  const israelTime = new Date(now.getTime() + offsetMinutes * 60000);
+
+  const hours = israelTime.getHours().toString().padStart(2, '0');
+  const minutes = israelTime.getMinutes().toString().padStart(2, '0');
   const time = `${hours}:${minutes}`;
-  
+
   return {
     statusCode: 200,
     headers: {
